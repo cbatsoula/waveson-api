ecc5c73943b8ee5e1279f38f85c4759e18abd37e
cbatsoula <cbatsoula@gmail.com>
2019-08-13 14:37:31 -0400

    mvp reached

---
+++ app/controllers/favs_controller.rb
@@ -0,0 +1,24 @@
+class FavsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def show
+    fav = Fav.find(params[:id])
+    render json: fav
+  end
+
+  def index
+    favs = Fav.all
+    render json: favs
+  end
+
+  def create
+    fav = Fav.new(user_id: params[:user_id], beach_id: params[:beach_id])
+
+    if fav.save
+      render json: fav
+    else
+      render json: {errors: fav.errors.full_messages}
+    end
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   resources :beaches
+  resources :favs
 
   patch 'users/:id', to: "users#update"
   post '/signup', to: "users#create"
