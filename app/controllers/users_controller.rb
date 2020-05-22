ccd7df9f863469bef44cb6ecda34ad59d43ef3d3
cbatsoula <cbatsoula@gmail.com>
2020-05-22 14:04:00 -0400

    db renders my one user, wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApiController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
   def index
     users = User.all
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :beaches
-  resources :favs
-  resources :notes
-  resources :tags
 
-  resources :note_tags
+  scope '/api' do
+    resources :users
+    resources :beaches
+    resources :favs
+    resources :notes
+    resources :tags
+
+    resources :note_tags
+  end
 
   patch '/users/:id', to: "users#update"
   delete '/favs/:id', to: "favs#destroy"
@@ -18,5 +21,5 @@ Rails.application.routes.draw do
   patch '/notes/:id', to: "notes#update"
 
   post 'beaches', to: "beaches#create"
-  
+
 end
