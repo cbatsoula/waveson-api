ca0c92b415cec803accd5b973472eeb4027742d4
cbatsoula <cbatsoula@gmail.com>
2020-06-11 14:32:18 -0400

    fixing the boxy weaher render in BeachDetails

--- client/src/containers/BeachDetails.js
+++ client/src/containers/BeachDetails.js
@@ -90,6 +90,7 @@ class BeachDetails extends React.Component {
       </div>
 
       <div className="Beach-Details">
+      <p>Today's General Weather</p>
         <ul>
           <li>Today's high: {this.props.today.maxtempF} F / {this.props.today.maxtempC} C</li>
           <li>Today's low: {this.props.today.mintempF} F / {this.props.today.mintempC} C</li>
@@ -101,11 +102,11 @@ class BeachDetails extends React.Component {
           <li>Sunrise: {this.props.today.astronomy[0].sunrise}</li>
 
         </ul>
-        <div className="Hourly">
-          {this.renderWeather()}
         </div>
-
+      <div className="Hourly">
+        {this.renderWeather()}
       </div>
+
       </>
     )
   }
