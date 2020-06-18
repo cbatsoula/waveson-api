e50757b73e06b88fc184cd39a8dfc659cdaf7489
cbatsoula <cbatsoula@gmail.com>
2020-06-18 17:23:32 -0400

    wip --i dont think ive done a drop down before

--- client/src/containers/AllNotesFilter.js
+++ client/src/containers/AllNotesFilter.js
@@ -10,35 +10,35 @@ class AllNotesFilter extends React.Component {
 
   }
 
-  changeOption = () => {
-    console.log("hi hello")
+  handleChange = (event) => {
+    this.setState({value: event.target.value});
   }
 
-  handleSubmit = () => {
-    console.log("hello hi")
+  handleSubmit = (event) => {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
   }
+
+  sortedList = () => {
+    this.props.allBeaches.map((location, index) => <option key={index}>{location}</option>);
+  }
+
 
   render () {
 
+
     return (
       <div className="AllNotesFilter-Container">
-      {
-        this.props.allBeaches
-        ?
-        <>
+
         <form onSubmit={this.handleSubmit}>
         <h3>Filter by...</h3>
         <label>Beach</label>
-        <select id="nation" value={this.props.allBeaches} onChange={this.changeOption} >
-         <option value="Name">"Name"</option>
-        </select>
+          <select value={this.props.allBeaches} onChange={this.handleChange} >
+           {this.sortedList()}
+          </select>
         <input type="submit" value="Submit" />
         </form>
-        </>
-        :
-        null
 
-      }
       </div>
 
     )
