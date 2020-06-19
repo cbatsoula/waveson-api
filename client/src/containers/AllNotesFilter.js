6fa358d6f6db46d992c59165b4cb582b92ca96d3
cbatsoula <cbatsoula@gmail.com>
2020-06-19 15:05:15 -0400

    selecting a beach fully works, now to dynamically render and do the same with timeslots

--- client/src/containers/AllNotesFilter.js
+++ client/src/containers/AllNotesFilter.js
@@ -4,29 +4,37 @@ import NoteCard from '../components/NoteCard';
 class AllNotesFilter extends React.Component {
 
   state = {
-
+    selectBeach: null,
   }
   componentDidMount = () => {
 
   }
 
   handleChange = (event) => {
-    this.setState({value: event.target.value});
+    console.log("event", event.target)
+    this.setState({
+      selectBeach: event.target.value
+    }, () => {console.log("handleChange", this.state)});
+
   }
 
   handleSubmit = (event) => {
-    alert('Your favorite flavor is: ' + this.state.value);
+    // alert('Beach selected: ' + this.state.value);
     event.preventDefault();
+    console.log("submit!!", this.state);
+    // this.setState({
+    //   selectBeach: event.target.value
+    // });
   }
 
   sortedList = () => {
-    return this.props.allBeaches.map(beach => <option key={beach.id}>{beach.name}</option>);
+    return this.props.allBeaches.map(beach => <option key={beach.id} value={beach.name} onChange={this.handleChange}>{beach.name}</option>);
   }
   // <option key={index}>{location}</option>
 
 
   render () {
-
+    console.log("render state", this.state)
 
     return (
       <div className="AllNotesFilter-Container">
@@ -34,7 +42,7 @@ class AllNotesFilter extends React.Component {
         <form onSubmit={this.handleSubmit}>
         <h3>Filter by...</h3>
         <label>Beach</label>
-          <select value={this.props.allBeaches} onChange={this.handleChange} >
+          <select value={this.state.selectBeach} onChange={this.handleChange} >
            {this.sortedList()}
           </select>
         <input type="submit" value="Submit" />
